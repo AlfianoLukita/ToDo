503640eb64405bf4b935dab4222665cb7b02ba8d
_AlfianoLukita <alfianolukita@gmail.com>
2026-01-16 23:03:45 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 console.log("js read")
-
+read()
 function Add() {
      let input = document.getElementById("Textinput");
      let task = input.value.trim();
@@ -10,8 +10,51 @@ function Add() {
        
        document.getElementById("list").appendChild(li);
        let del = document.createElement("span");
-       del.innerHTML = "X";
-       li.appendChild(del)
+       del.innerHTML = "";
+       del.style.cursor =  "pointer";
+      
+       del.onclick = function(){
+         li.remove();
+         console.log("remove");
+         save()
+       };
+       li.addEventListener("click", () => {
+         li.classList.toggle("done")
+         save()
+      });
+       
+           
+       
+       li.appendChild(del);
+       console.log("task Added");
+       save()
        input.value = "";
      }
+}
+function save(){
+  let list = document.getElementById('list').innerHTML ;
+  localStorage.setItem("data" ,list);
+}
+function event(){
+  let item = document.querySelectorAll("#list li")
+  item.forEach(li=>{
+    let del = li.querySelector("span");
+    del.onclick = function(e){
+      e.stopPropagation();
+      li.remove();
+      save()
+      console.log("remove");
+    };
+    li.addEventListener("click", () => {
+     li.classList.toggle("done")
+     save()
+    });
+  });
+}
+function read(){
+  let data = localStorage.getItem("data");
+  if (data) {
+    document.getElementById("list").innerHTML = data;
+  }
+  event();
 }
